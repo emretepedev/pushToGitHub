4a23d023378f15c14145d86f485bbc6e318f2471
emretepedev <emretepedev@gmail.com>
2022-02-15 20:19:11 +0300

    added commit based transaction

--- pushToGitHub.sh
+++ pushToGitHub.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 firstPWD=$PWD
+
+# path of repo which is one on github
 repoPath="$HOME/pushToGitHub/"
+
+# switch dir to push
 cd $repoPath
 
+# push
 git push
 
+# back to the first dir
 cd $firstPWD
