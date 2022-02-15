de7f7cce7688074217aef47571b8081dfb360e31
Emre Tepe <emretepedev@gmail.com>
2022-02-15 20:19:11 +0300

    added commit based transaction

---
+++ commitToGitHub.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+firstPWD=$PWD
+repoPath="$HOME/pushToGitHub/"
+cd $repoPath
+
+filePath="./someChanges.txt"
+
+if ! [ -f $filePath ]
+then
+    touch $filePath
+    echo 0 > $filePath
+fi
+
+oldValue=$( cat $filePath )
+
+if [ -z $oldValue ]
+then
+    echo 0 > $filePath
+    git commit --allow-empty-message -am ''
+    exit
+fi
+
+numberRegex="^[[:digit:]]+$"
+maxValue=$(( (2 ** 16) - 1 ))
+if [[ $oldValue =~ $numberRegex && $oldValue -lt $maxValue ]]
+then
+    newValue=$(( ${oldValue} + 1 ))
+else
+    newValue=0
+fi
+
+sed -i "s/$oldValue/$newValue/g" $filePath
+
+git commit --allow-empty-message -am ''
+
+cd $firstPWD
