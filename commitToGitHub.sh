1a3c56194f7d2e1ea10815e25cd4d28294049a3d
emretepedev <emretepedev@gmail.com>
2025-08-23 16:19:28 +0300

    Refactor cleanup function in commitToGitHub.sh and init.sh for improved hooks management. Ensure original hooks path is restored correctly upon script exit.

--- commitToGitHub.sh
+++ commitToGitHub.sh
@@ -6,6 +6,13 @@ FIRST_DIR=$PWD
 # path of the repository
 REPO_DIR="$HOME/pushToGitHub"
 
+cleanup() {
+  # restore original hooks path if it exists
+  if [ -n "$ORIGINAL_HOOKS_PATH" ]; then
+    git config --global core.hooksPath "$ORIGINAL_HOOKS_PATH"
+  fi
+}
+
 # cleanup function will be called when the script exits
 trap cleanup EXIT
 
@@ -13,22 +20,15 @@ trap cleanup EXIT
 cd "$REPO_DIR" || (echo "your repository path is not correct" && exit)
 
 # disable global hooks for this commit
+ORIGINAL_HOOKS_PATH=$(git config --global --get core.hooksPath)
 git config --global --unset-all core.hooksPath
 
 # get user name and email
-GIT_USER_NAME=$(git config user.name)
-GIT_USER_EMAIL=$(git config user.email)
+GIT_USER_NAME=$(git config --get user.name)
+GIT_USER_EMAIL=$(git config --get user.email)
 
 # take a commit
 git commit -S --allow-empty --allow-empty-message --file /dev/null --author="$GIT_USER_NAME <$GIT_USER_EMAIL>"
 
 # back to first dir
 cd "$FIRST_DIR" || echo "your first path is not correct"
-
-cleanup() {
-  if [ -n "$original_hooks_path" ]; then
-    git config --global core.hooksPath "$original_hooks_path"
-  else
-    git config --global --unset-all core.hooksPath
-  fi
-}
--- git-hooks-examples/husky/init.sh
+++ git-hooks-examples/husky/init.sh
@@ -5,14 +5,6 @@ GLOBAL_HOOKS_DIR=$(git config --global --get core.hooksPath)
 GLOBAL_HOOK="${GLOBAL_HOOKS_DIR%/}/${HOOK}"
 SHOW_HOOK_MESSAGE=true
 
-cleanup() {
-  if [ -n "$GLOBAL_HOOKS_DIR" ]; then
-    git config --global core.hooksPath "$GLOBAL_HOOKS_DIR"
-  fi
-}
-
-trap cleanup EXIT
-
 if [ -n "$GLOBAL_HOOKS_DIR" ] && [ -f "$GLOBAL_HOOK" ]; then
   if [ "$SHOW_HOOK_MESSAGE" = true ]; then
     echo "Global hook found for $HOOK"
