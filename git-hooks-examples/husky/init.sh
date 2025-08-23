cdea9ed8dc95a2c18ff156c8a3b9ab605880a768
emretepedev <emretepedev@gmail.com>
2025-08-24 01:59:39 +0300

    Refactor Git hooks to streamline project and global hook execution in init.sh, post-commit, and pre-push scripts. Remove redundant code and improve clarity in hook management.

--- git-hooks-examples/husky/init.sh
+++ git-hooks-examples/husky/init.sh
@@ -1,16 +1,46 @@
 #!/bin/sh
 
 HOOK=$(basename "$0")
+PROJECT_HOOKS_DIR_RAW=$(git config --get core.hooksPath)
 GLOBAL_HOOKS_DIR=$(git config --global --get core.hooksPath)
 GLOBAL_HOOK="${GLOBAL_HOOKS_DIR%/}/${HOOK}"
 SHOW_HOOK_MESSAGE=true
 
-if [ -n "$GLOBAL_HOOKS_DIR" ] && [ -f "$GLOBAL_HOOK" ]; then
-  if [ "$SHOW_HOOK_MESSAGE" = true ]; then
-    echo "Global hook found for $HOOK"
+PROJECT_HOOKS_DIR="${PROJECT_HOOKS_DIR_RAW%/_}"
+PROJECT_HOOK="${PROJECT_HOOKS_DIR%/}/${HOOK}"
+
+run_project_hook_if_exists() {
+  if [ -f "$PROJECT_HOOK" ]; then
+    if [ "$SHOW_HOOK_MESSAGE" = true ]; then
+      echo "Project hook found for $HOOK"
+    fi
+
+    sh "$PROJECT_HOOK"
+
+    EXIT_CODE=$?
+    if [ $EXIT_CODE -ne 0 ]; then
+      exit $EXIT_CODE
+    fi
   fi
+}
 
-  sh "$GLOBAL_HOOK" "$@"
+run_global_hook_if_exists() {
+  if [ -n "$GLOBAL_HOOKS_DIR" ] && [ -f "$GLOBAL_HOOK" ]; then
+    if [ "$SHOW_HOOK_MESSAGE" = true ]; then
+      echo "Global hook found for $HOOK"
+    fi
 
-  exit $?
-fi
+    sh "$GLOBAL_HOOK"
+
+    EXIT_CODE=$?
+    if [ $EXIT_CODE -ne 0 ]; then
+      exit $EXIT_CODE
+    fi
+  fi
+}
+
+run_project_hook_if_exists
+
+run_global_hook_if_exists
+
+exit 0
